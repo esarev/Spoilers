33158ea2c6c2219b32c7c1579d4880ff73bbedf8
Alex <esarev@inbox.ru>
2021-10-21 22:36:35 +0700

    js_mistake

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@
 /*Spoilers*/
 
 const spoilersArray = document.querySelectorAll('[data-spoilers]');
+
+function initSpoilers(spoilersArray, matchMedia = false) {
+    spoilersArray.forEach(spoilersBlock => {
+      spoilersBlock = matchMedia ? spoilersBlock.item : spoilersBlock;
+      if (matchMedia.matches || !matchMedia) {
+        spoilersBlock.classList.add('_init');
+        initSpoilerBody(spoilersBlock);
+        spoilersBlock.addEventListener('click', setSpoilerAction);
+      }else {
+        spoilersBlock.classList.remove('_init');
+        initSpoilerBody(spoilersBlock, false);
+        spoilersBlock.removeEventListener('click', setSpoilerAction);
+      }
+    });
+  }
+
 if (spoilersArray.length > 0) {
   //Получение обычных спойлеров(также через Array переводим коллекцию в массив)
   const spoilersRegular = Array.from(spoilersArray).filter(function(item, index, self) {
@@ -19,6 +35,8 @@ if (spoilersArray.length > 0) {
   });
 
   //Инициализация спойлеров с медиа запросами 
+  
+
   if (spoilersMedia.length > 0) {
     const breakpointsArray = [];
     spoilersMedia.forEach(item => {
@@ -77,6 +95,7 @@ if (spoilersArray.length > 0) {
       }
     });
   }
+  
 
   //Работа с контентом
   function initSpoilerBody(spoilersBlock, hideSpoilerBody = true) {
@@ -108,7 +127,7 @@ if (spoilersArray.length > 0) {
           hideSpoilersBody(spoilersBlock);
         }
         spoilerTitle.classList.toggle('_active');
-        _slideTooggle(spoilerTitle.nextElementSibling, 500);
+        _slideToggle(spoilerTitle.nextElementSibling, 500);
       }
       e.preventDefault();
     }
@@ -128,4 +147,86 @@ if (spoilersArray.length > 0) {
 
 
 
+
 /*SlideToggle*/
+
+let _slideUp = (target, duration = 500) => {
+
+  if(!target.classList.contains('_slide')) {
+    target.classList.add('_slide');
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+      target.hidden = true;
+      target.style.removeProperty('height');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+      target.classList.remove('_slide');
+    }, duration);
+
+  }
+
+};
+
+let _slideDown = (target, duration = 500) => {
+
+  if(!target.classList.contains('_slide')) {
+    target.classList.add('_slide');
+    if(target.hidden) {
+      target.hidden = false;
+    }
+
+    let height = target.offsetHeight;
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout(() => {
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+      target.classList.remove('_slide');
+    }, duration);
+
+  }
+
+};
+
+let _slideToggle = (target, duration = 500) => {
+
+  if(target.hhidden) {
+    return _slideDown(target, duration);
+  }else {
+    return _slideUp(target, duration);
+  }
+
+};
+
+
+
+
+
